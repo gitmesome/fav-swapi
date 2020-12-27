4dd2c34084c106cfaa269ea0ac18fe6a8a978d27
Christian Ullman <christian.ullman@plansource.com>
2020-12-26 23:50:39 -0700

    Change the logo svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import logo from './new-replubic.svg';
 import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      character: {}
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch("https://swapi.dev/api/people/1")
+      .then(response =>  response.json())
+      .then(data => {
+          this.setState({
+            character: data
+          })
+        }
+      )
+  }
+  
+  handleChange(event){
+    const {name, value, type, checked} = event.target;
+    type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> AND save to reload now.
+          </p>
+          <p>
+            We got {this.state.character.name}
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
