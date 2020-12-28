0249232046ebedc085b97c85f19e4e58e4a21ee4
Christian Ullman <christian.ullman@plansource.com>
2020-12-27 17:37:19 -0700

    Remove some artifiacts left over from "yarn create react-app"

--- src/App.js
+++ src/App.js
@@ -50,24 +50,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> AND save to reload now.
-          </p>
-          <p>
-            <SearchPerson searchForPerson = { this.searchForPerson } />
-          </p>
-          <p>
-            We got {this.state.character.name}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <p>
+          <SearchPerson searchForPerson = { this.searchForPerson } />
+        </p>
       </div>
     );
   }
